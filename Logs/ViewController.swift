c80f236db76827bb20fdc733f4decb959c1bf224
Gubba Manoj Kumar <manoj.gubba@gmail.com>
2020-05-02 22:20:02 +0530

    create basic view and add EasyPeasy framework

--- Logs/ViewController.swift
+++ Logs/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import EasyPeasy
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.text = "Hello World"
+        view.addSubview(label)
+        label.easy.layout([
+            Center(0)
+        ])
+    }
 }
 
