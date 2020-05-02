38751c03beec20a1f72c4ee62b392ecd2d6f398a
Gubba Manoj Kumar <manoj.gubba@gmail.com>
2020-05-02 20:35:14 +0530

    Initial Commit

---
+++ Logs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logs
+//
+//  Created by manoj.gubba on 2020/05/02.
+//  Copyright © 2020 manoj.gubba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
