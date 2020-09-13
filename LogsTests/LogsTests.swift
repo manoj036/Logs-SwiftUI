31fb3dca61496755ec4d9f13b5948b1b02ff25e8
Gubba Manoj Kumar <manoj.gubba@gmail.com>
2020-09-13 11:21:02 +0530

    test workflow with failing tests

---
+++ LogsTests/LogsTests.swift
@@ -0,0 +1,32 @@
+//
+//  LogsTests.swift
+//  LogsTests
+//
+//  Created by manoj.gubba on 2020/09/13.
+//  Copyright © 2020 manoj.gubba. All rights reserved.
+//
+
+import XCTest
+
+class LogsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(1+1, 3)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
